5260022bfec472b7b55501ea5655b6919c2ffa2c
Edgar Longat <64935800+longate58@users.noreply.github.com>
2022-01-13 14:30:26 -0400

    Delete js directory

--- js/vecinos.js
+++
@@ -1,5 +0,0 @@
-var vecinos = L.geoJson(vecinos,{
-  onEachFeature: function(feature, layer){
-    layer.bindPopup(feature.properties["nombre"] + '<br>'+ '(' + feature.properties["direccion"] + ')')
-  }
-}).addTo(map); 
